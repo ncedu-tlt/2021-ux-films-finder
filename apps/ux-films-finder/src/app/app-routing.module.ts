93abcbc8ab6fc1c9922bf0bde64bf741b863bbc7
Ekaterina <savinova.katerina@inbox.ru>
2022-02-09 20:34:44 +0400

    added routing module

---
+++ apps/ux-films-finder/src/app/app-routing.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';
+import { AdvancedSearchComponent } from './components/advanced-search/advanced-search.component';
+import { CategoryComponent } from './components/category/category.component';
+import { DetailsComponent } from './components/details/details.component';
+import { NotFoundPageComponent } from './components/not-found-page/not-found-page.component';
+import { SearchPersonsComponent } from './components/search-persons/search-persons.component';
+import { MainPageComponent } from './components/main-page/main-page.component';
+import { SimpleExComponent } from './components/simple-ex/simple-ex.component';
+const routes: Routes = [
+  {path: '', component: MainPageComponent},
+  {path: 'search-films', component: AdvancedSearchComponent},
+  {path: 'category', component: CategoryComponent},
+  {path: 'search-persons', component: SearchPersonsComponent},
+  {path: 'details/:id', component: DetailsComponent},
+  {path: 'simple-ex', component: SimpleExComponent},
+  {path: '**', component: NotFoundPageComponent}
+];
+@NgModule({
+  imports: [BrowserModule, RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
