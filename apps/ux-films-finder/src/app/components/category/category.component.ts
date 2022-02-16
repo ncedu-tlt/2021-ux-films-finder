8bc83f9e6de28074a8888c913df50890b9fcc88f
Ekaterina <savinova.katerina@inbox.ru>
2022-02-16 16:50:30 +0400

    added category

--- apps/ux-films-finder/src/app/components/category/category.component.ts
+++ apps/ux-films-finder/src/app/components/category/category.component.ts
@@ -10,15 +10,15 @@ import { MatPaginatorIntl } from '@angular/material/paginator';
 export class PaginatorIntl implements MatPaginatorIntl {
   changes = new Subject<void>();
 
-  firstPageLabel = `First page`;
-  itemsPerPageLabel = `Items per page:`;
-  lastPageLabel = `Last page`;
+  firstPageLabel = 'First page';
+  itemsPerPageLabel = 'Items per page:';
+  lastPageLabel = 'Last page';
   nextPageLabel = 'Next page';
   previousPageLabel = 'Previous page';
 
   getRangeLabel(page: number, pageSize: number, length: number): string {
     if (length === 0) {
-      return `Page 1 of 1`;
+      return 'Page 1 of 1';
     }
     const amountPages = Math.ceil(length / pageSize);
     return `Page ${page + 1} of ${amountPages}`;
