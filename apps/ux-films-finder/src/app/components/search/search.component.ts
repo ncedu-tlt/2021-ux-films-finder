90ded1ea322a8b493469c701b580ffa1fb29f259
Daniil <chugunov1d@gmail.com>
2022-02-21 20:08:00 +0400

    search

---
+++ apps/ux-films-finder/src/app/components/search/search.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'ff-search',
+  templateUrl: './search.component.html',
+  styleUrls: ['./search.component.less']
+})
+export class SearchComponent {}
