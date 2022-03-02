e9e1a6bdbef2650d31d17cf54b14408ac8a58390
Ekaterina <savinova.katerina@inbox.ru>
2022-03-02 18:17:32 +0400

    redirect to search component from person component

--- apps/ux-films-finder/src/app/components/person/person.component.ts
+++ apps/ux-films-finder/src/app/components/person/person.component.ts
@@ -25,4 +25,50 @@ export class PersonComponent implements OnInit {
         });
     });
   }
+
+  public getRole(professionKey: string): string {
+    switch (professionKey) {
+      case 'ACTOR':
+        return 'Актёр';
+
+      case 'OPERATOR':
+        return 'Оператор';
+
+      case 'HRONO_TITR_MALE':
+        return 'Титры хроно мужской';
+
+      case 'HRONO_TITR_FEMALE':
+        return 'Титры хроно женский';
+
+      case 'VOICE_FEMALE':
+        return 'Озвучка женский';
+
+      case 'VOICE_MALE':
+        return 'Озвучка мужской';
+
+      case 'PRODUCER':
+        return 'Продюссер';
+
+      case 'HIMSELF':
+        return 'Сам себя';
+
+      case 'WRITER':
+        return 'Писатель';
+
+      case 'EDITOR':
+        return 'Редактор';
+
+      case 'DESIGN':
+        return 'Дизайнер';
+
+      case 'HERSELF':
+        return 'Сама себя';
+
+      case 'DIRECTOR':
+        return 'Режиссер';
+
+      default:
+        return 'Не указано';
+    }
+  }
 }
--- apps/ux-films-finder/src/app/components/search-persons/search-persons.component.ts
+++ apps/ux-films-finder/src/app/components/search-persons/search-persons.component.ts
@@ -1,22 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FilmDataService } from '../../services/film-data.service';
 import { PersonInfoResponseModel } from '../../models/person-info-response.model';
 import { Subject, Subscription, take } from 'rxjs';
-import { PersonInfoModel } from '../../models/person-info.model';
 import { PageEvent } from '@angular/material/paginator';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'ff-search-persons',
   templateUrl: './search-persons.component.html',
   styleUrls: ['./search-persons.component.less']
 })
-export class SearchPersonsComponent {
-  constructor(private personData: FilmDataService) {}
+export class SearchPersonsComponent implements OnInit {
+  constructor(
+    private personData: FilmDataService,
+    private router: Router,
+    private activatedRoute: ActivatedRoute
+  ) {}
   public person$: Subject<PersonInfoResponseModel> =
     new Subject<PersonInfoResponseModel>();
   readonly pageSize = 50;
   private loadInfo$: Subscription = new Subscription();
   search = '';
+  ngOnInit(): void {
+    this.activatedRoute.queryParams.subscribe(params => {
+      this.search = params['search'];
+    });
+    this.loadPersonInfo(1);
+  }
   loadPersonInfo(pageIndex: number) {
     this.loadInfo$ && this.loadInfo$.unsubscribe();
     this.loadInfo$ = this.personData
@@ -27,6 +37,9 @@ export class SearchPersonsComponent {
       });
   }
   onSearch() {
+    this.router.navigate(['/search-persons'], {
+      queryParams: { search: this.search }
+    });
     this.loadPersonInfo(1);
   }
   onPageChange(page: PageEvent): void {
