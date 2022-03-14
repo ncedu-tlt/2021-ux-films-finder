bf0e69ab89d5607102ba6b91607987cc47b1f04c
Gavlonskaya Ekaterina <gavlonskaya.katerina.yandex.ru>
2022-03-14 11:08:43 +0300

    add color const

---
+++ apps/ux-films-finder/src/app/consts/color-gender.const.ts
@@ -0,0 +1,21 @@
+export const COLORS: {
+  [name: string]: string;
+} = {
+  комедия: '_bg-comedy',
+  ужасы: '_bg-horror',
+  приключения: '_bg-adventures',
+  триллер: '_bg-thriller',
+  мелодрама: '_bg-melodrama',
+  фэнтези: '_bg-fantasy',
+  боевик: '_bg-action',
+  семейный: '_bg-family',
+  драма: '_bg-drama',
+  криминал: '_bg-crime',
+  мультфильм: '_bg-cartoon',
+  аниме: '_bg-anime',
+  военный: '_bg-military',
+  история: '_bg-history',
+  биография: '_bg-biography',
+  детектив: '_bg-detective',
+  фантастика: '_bg-fiction'
+};
