b03b524a476bf7d3985a29b9709d2d07c9cb3fa2
Gavlonskaya Ekaterina <gavlonskaya.katerina.yandex.ru>
2022-04-18 08:55:48 +0300

    new title-name.const.ts title-films-list

--- apps/ux-films-finder/src/app/components/title-films-list/title-films-list.component.ts
+++ apps/ux-films-finder/src/app/components/title-films-list/title-films-list.component.ts
@@ -14,20 +14,8 @@ export class TitleFilmsListComponent {
   public color = COLORSTITLE;
   public titleGenres = '';
   public name = COLORSTITLENAME;
-  // private routerSubscription: Subscription;
-
   constructor(private router: Router) {}
-  //   this.routerSubscription = router.params.subscribe(params=>this.id=params['']);
-  //   this
-  // .
-  //   router
-  // .
-  //
-  //   navigate(['localhost:4200'] {
-  //
-  //   }
-  //
-  // )
+
   ngOnInit() {
     this.titleGenres = this.router.url;
     console.log(this.router.url);
